e412f35b625caf8457cf59cf974aedb345f6faa5
nohsueh <hsuehhsiang@163.com>
2022-11-03 19:08:15 +0800

    format

--- src/pages/Apps/PhotoViewer/PhotoViewer.js
+++ src/pages/Apps/PhotoViewer/PhotoViewer.js
@@ -1,19 +1,58 @@
-import { DatePicker, Image, Space, BackTop } from 'antd';
+import { DatePicker, Image, Space, BackTop, Cascader } from 'antd';
 import React, { useState, useEffect } from 'react';
 import Navigation from '../../../components/Header/Navigation';
 import './PhotoViewer.css';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://43.143.171.145:8080';
-const baseURL = 'http://43.143.171.145:8080';
 
-
-
+const options = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
 
 export default function PhotoViewer() {
+  useEffect(() => {
+    loadPhoto(null, null);
+    return;
+  }, []);
+  
   const [photos, setPhotos] = useState([]);
 
   const loadPhoto = (date, dateString) => {
+    console.log('date'+date)
+    console.log('dateString'+dateString)
+
     axios.get('/loadPhoto', {
       params: {
         time: dateString,
@@ -30,20 +69,15 @@ export default function PhotoViewer() {
       });
   };
 
-  useEffect(() => {
-    loadPhoto(null, null);
-    return;
-  }, []);
-
   let Images = photos.map((photo, index) => {
     return (
       <Image
         loading='lazy'
         key={index}
         height={180}
-        width={177}
-        src={baseURL + photo.file}
-        style={{ borderRadius: '7%', padding: '4px 4px 4px 4px', }}
+        width={174}
+        src={axios.defaults.baseURL + photo.file}
+        style={{ borderRadius: '7%', padding: '3px', }}
       />
     );
   })
@@ -51,8 +85,12 @@ export default function PhotoViewer() {
   return (
     <div>
       <Navigation />
-      <Space direction='vertical' style={{ margin: '20px 20px 20px 20px', }}>
-        <DatePicker onChange={loadPhoto} style={{ marginLeft: '50px', }} />
+      <Space direction='vertical' style={{ margin: '30px', }}>
+        <Space>
+          <DatePicker onChange={loadPhoto}  style={{ margin: '11.5px', }}/>
+          <Cascader options={options} onChange={loadPhoto} placeholder="Please select" />
+        </Space>
+
         <Image.PreviewGroup >
           {Images}
         </Image.PreviewGroup>
